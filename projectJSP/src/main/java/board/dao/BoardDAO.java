4ec7e455998d86e5da3d18732dbc83aa184d2da7
SOOYEON21428 <joenpark06@naver.com>
2024-02-18 13:39:10 +0900

    projectJSP update

--- projectJSP/src/main/java/board/dao/BoardDAO.java
+++ projectJSP/src/main/java/board/dao/BoardDAO.java
@@ -1,54 +1,45 @@
 package board.dao;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-
+import board.bean.BoardDTO;
+import org.apache.ibatis.exceptions.PersistenceException;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import board.bean.BoardDTO;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class BoardDAO {
-
     private SqlSessionFactory sqlSessionFactory;
 
     private static BoardDAO boardDAO = new BoardDAO();
+    public static BoardDAO getInstance() { return boardDAO; }
 
-    public static BoardDAO getInstance() {
-        return boardDAO;
-    }
-
-    private BoardDAO() {
+    public BoardDAO(){
         try {
             String resource = "mybatis-config.xml";
             InputStream inputStream = Resources.getResourceAsStream(resource);
             sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        } catch (IOException e) {
+        } catch (IOException e){
             e.printStackTrace();
         }
     }
 
-    // 게시글 작성
-    public void boardWrite(String id, String name, String email, String subject, String content) {
-        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-            sqlSession.insert("boardSQL.boardWrite", new BoardDTO(id, name, email, subject, content));
+    public boolean makeBoard(Map<String, String> map){
+        try {
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+            sqlSession.insert("boardSQL.makeBoard", map);
             sqlSession.commit();
-        }
-    }
-
-    // 게시물 출력
-    public ArrayList<BoardDTO> loadContents(int startNum, int endNum) {
-        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-            return new ArrayList<>(sqlSession.selectList("boardSQL.loadContents", new BoardDTO(startNum, endNum)));
-        }
-    }
-
-    public int getTotalA() {
-        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-            return sqlSession.selectOne("boardSQL.getTotalA");
+            sqlSession.close();
+            return true;
+        }catch(PersistenceException e){
+            e.printStackTrace();
+            return false;
         }
     }
 }
--- projectJSP/src/main/java/member/dao/MemberDAO.java
+++ projectJSP/src/main/java/member/dao/MemberDAO.java
@@ -1,54 +1,57 @@
-package member.dao;
+    package member.dao;
 
-import java.io.IOException;
+    import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+    import java.util.Map;
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
+    import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import member.bean.MemberDTO;
 
-public class MemberDAO {
-    private SqlSessionFactory sqlSessionFactory;
+    public class MemberDAO {
+        private SqlSessionFactory sqlSessionFactory;
 
-    private static MemberDAO memberDAO = new MemberDAO();
+        private static MemberDAO memberDAO = new MemberDAO();
 
-    public static MemberDAO getInstance() {
-        return memberDAO;
-    }
+        public static MemberDAO getInstance() {
+            return memberDAO;
+        }
 
-    private MemberDAO() {
-        try {
-            String resource = "mybatis-config.xml";
-            InputStream inputStream = Resources.getResourceAsStream(resource);
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
+        private MemberDAO() {
+        	try {
+                String resource = "mybatis-config.xml";
+                InputStream inputStream = Resources.getResourceAsStream(resource);
+                sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        
+        }
+
+        public boolean isExistID(String id) {
+            try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+                return sqlSession.selectOne("memberSQL.isExistID", id);
+            }
+        }
+
+        public boolean insert(MemberDTO memberDTO) {
+            try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+                sqlSession.insert("memberSQL.insert", memberDTO);
+                sqlSession.commit();
+                return true;
+            }
+        }
+
+        public MemberDTO login(String id, String pwd) {
+            try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+                Map<String, String> map = new HashMap<>();
+                map.put("id", id);
+                map.put("password", pwd);
+                return sqlSession.selectOne("memberSQL.login", map);
+            }
         }
     }
-
-    public boolean isExistID(String id) {
-        boolean exist = false;
-        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-            exist = sqlSession.selectOne("memberSQL.isExistID", id) != null;
-        }
-        return exist;
-    }
-
-    public boolean insert(MemberDTO memberDTO) {
-        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-            sqlSession.insert("memberSQL.insert", memberDTO);
-            sqlSession.commit();
-        }
-        return true;
-    }
-
-    public MemberDTO login(String id, String pwd) {
-        MemberDTO memberDTO = null;
-        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-            memberDTO = sqlSession.selectOne("memberSQL.login", new MemberDTO(id, pwd));
-        }
-        return memberDTO;
-    }
-}
